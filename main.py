e07649494d76ecb61c1c6d19d24508ed1aad4f31
fatih-iver <fatih.iver@boun.edu.tr>
2019-09-30 22:00:16 +0300

    get ipv4 and network addresses

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("NetChat")
+import os
+import socket
+import sys
+print("Welcome to NetChat!")
+
+# HOST_IPV4_ADDRESS = socket.gethostbyname(socket.gethostname())
+
+
+def get_host_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+host_ip_address = get_host_ip_address()
+
+
+def ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except socket.error:
+        return False
+    return True
+
+
+if not ipv4_address(host_ip_address):
+    print("Host ip address [", host_ip_address, "] is not an ipv4 address, program terminated!")
+    sys.exit()
+
+host_ipv4_address = host_ip_address
+
+print("HOST_IPV4_ADDRESS", host_ip_address)
+
+host_ipv4_network_address = host_ip_address[:host_ip_address.rfind(".")+1]
+
+print("HOST_IPV4_NETWORK_ADDRESS", host_ipv4_network_address)
+
+#os.system("ncat -l 127.0.0.1 12345")
